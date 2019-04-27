700cbd570e50987dd8b69f5c267f109dbda2ca2b
Manabu Niseki <manabu.niseki@gmail.com>
2019-04-28 08:28:42 +0900

    feat: add urlscan.io link to the Slack notifiaction

--- lib/mihari/notifiers/slack.rb
+++ lib/mihari/notifiers/slack.rb
@@ -2,10 +2,13 @@
 
 require "slack/incoming/webhooks"
 require "digest/sha2"
+require "mem"
 
 module Mihari
   module Notifiers
     class Attachment
+      include Mem
+
       attr_reader :data, :data_type
 
       def initialize(data:, data_type:)
@@ -13,8 +16,53 @@ module Mihari
         @data_type = data_type
       end
 
+      def vt_link
+        return nil unless _vt_link
+
+        {
+          fallback: "VT link",
+          title: data,
+          title_link: _vt_link,
+          footer: "virustotal.com",
+          footer_icon: "http://www.google.com/s2/favicons?domain=virustotal.com"
+        }
+      end
+
+      def urlscan_link
+        return nil unless _urlscan_link
+
+        {
+          fallback: "urlscan.io link",
+          title: data,
+          title_link: _urlscan_link,
+          footer: "urlscan.io",
+          footer_icon: "http://www.google.com/s2/favicons?domain=urlscan.io"
+        }
+      end
+
+      # @return [Array]
+      def to_a
+        [vt_link, urlscan_link].compact
+      end
+
+      private
+
       # @return [String]
-      def link
+      def _urlscan_link
+        case data_type
+        when "ip"
+          "https://urlscan.io/ip/#{data}"
+        when "domain"
+          "https://urlscan.io/domain/#{data}"
+        when "url"
+          uri = URI(data)
+          "https://urlscan.io/domain/#{uri.hostname}"
+        end
+      end
+      memoize :_urlscan_link
+
+      # @return [String]
+      def _vt_link
         case data_type
         when "hash"
           "https://www.virustotal.com/#/file/#{data}"
@@ -26,23 +74,9 @@ module Mihari
           "https://www.virustotal.com/#/url/#{sha256}"
         when "mail"
           "https://www.virustotal.com/#/search/#{data}"
-        else
-          ""
         end
       end
-
-      # @return [Hash]
-      def to_h
-        {
-          fallback: "VT link",
-          title: data,
-          title_link: link,
-          footer: "virustotal.com",
-          footer_icon: "http://www.google.com/s2/favicons?domain=virustotal.com"
-        }
-      end
-
-      private
+      memoize :_vt_link
 
       # @return [String]
       def sha256
@@ -72,8 +106,8 @@ module Mihari
 
       def to_attachments(artifacts)
         artifacts.map do |artifact|
-          Attachment.new(data: artifact.data, data_type: artifact.data_type).to_h
-        end
+          Attachment.new(data: artifact.data, data_type: artifact.data_type).to_a
+        end.flatten
       end
 
       def notify(title:, description:, artifacts:)
