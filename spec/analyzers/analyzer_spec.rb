ba6d2325e595a8702f4b9af88456f494a2eb2cb5
Manabu Niseki <manabu.niseki@gmail.com>
2019-06-07 15:28:40 +0900

    refactor: add notificaton error handling

--- spec/analyzers/analyzer_spec.rb
+++ spec/analyzers/analyzer_spec.rb
@@ -35,5 +35,27 @@ RSpec.describe Mihari::Analyzers::Base, :vcr do
     it "doens't raise any error" do
       capture(:stdout) { subject.run }
     end
+
+    context "when a notifer raises an error" do
+      before do
+        notifier = double("notifier_instance")
+        allow(notifier).to receive(:valid?).and_return(true)
+        allow(notifier).to receive(:notify).and_raise("error")
+
+        klass = double("notifier_class")
+        allow(klass).to receive(:new).and_return(notifier)
+
+        allow(Mihari).to receive(:notifiers).and_return([klass])
+
+        thehive = instance_double(Mihari::TheHive)
+        allow(thehive).to receive(:valid?).and_return(false)
+        allow(Mihari::TheHive).to receive(:new).and_return(thehive)
+      end
+
+      it do
+        output = capture(:stdout){ subject.run }
+        expect(output).to include("Sending notification by")
+      end
+    end
   end
 end
