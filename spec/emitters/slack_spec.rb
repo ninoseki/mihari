3f1fb15c80ae0d5cacfbc19c94816aa66aba1efd
Manabu Niseki <manabu.niseki@gmail.com>
2019-08-12 15:24:22 +0900

    test: improve specs

--- spec/emitters/slack_spec.rb
+++ spec/emitters/slack_spec.rb
@@ -3,17 +3,17 @@
 RSpec.describe Mihari::Emitters::Slack do
   subject { described_class.new }
 
-  describe "#to_attachments" do
-    let(:artifacts) {
-      [
-        Mihari::Artifact.new("1.1.1.1"),
-        Mihari::Artifact.new("github.com"),
-        Mihari::Artifact.new("http://example.com"),
-        Mihari::Artifact.new("44d88612fea8a8f36de82e1278abb02f"),
-        Mihari::Artifact.new("example@gmail.com")
-      ]
-    }
+  let(:artifacts) {
+    [
+      Mihari::Artifact.new("1.1.1.1"),
+      Mihari::Artifact.new("github.com"),
+      Mihari::Artifact.new("http://example.com"),
+      Mihari::Artifact.new("44d88612fea8a8f36de82e1278abb02f"),
+      Mihari::Artifact.new("example@gmail.com")
+    ]
+  }
 
+  describe "#to_attachments" do
     it do
       attachments = subject.to_attachments(artifacts)
       attachments.each do |a|
@@ -45,6 +45,25 @@ RSpec.describe Mihari::Emitters::Slack do
       it do
         expect(subject.valid?).to be(false)
       end
+    end
+  end
+
+  describe "#emit" do
+    let(:title) { "test" }
+    let(:description) { "test" }
+    let(:mock) { double("notifier") }
+
+    before do
+      allow(Slack::Notifier).to receive(:new).and_return(mock)
+      allow(mock).to receive(:post)
+
+      allow(subject).to receive(:slack_channel)
+      allow(subject).to receive(:slack_webhook_url)
+    end
+
+    it do
+      subject.emit(title: title, description: description, artifacts: artifacts)
+      expect(mock).to have_received(:post).once
     end
   end
 end
--- spec/emitters/the_hive_spec.rb
+++ spec/emitters/the_hive_spec.rb
@@ -25,4 +25,24 @@ RSpec.describe Mihari::Emitters::TheHive, :vcr do
       end
     end
   end
+
+  describe "#emit" do
+    let(:title) { "test" }
+    let(:description) { "test" }
+    let(:artifacts) { [Mihari::Artifact.new("1.1.1.1")] }
+
+    let(:mock_thehie) { double("thehive") }
+    let(:mock_alert) { double("alert") }
+
+    before do
+      allow(subject).to receive(:the_hive).and_return(mock_thehie)
+      allow(mock_thehie).to receive(:alert).and_return(mock_alert)
+      allow(mock_alert).to receive(:create)
+    end
+
+    it do
+      subject.emit(title: title, description: description, artifacts: artifacts)
+      expect(mock_alert).to have_received(:create)
+    end
+  end
 end
