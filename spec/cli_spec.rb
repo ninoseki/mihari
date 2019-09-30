a07892c2232f8ad04bed5e8ce2147aa53e77338c
Manabu Niseki <manabu.niseki@gmail.com>
2019-10-01 07:33:00 +0900

    feat: add DNPedia lookup

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -80,6 +80,15 @@ RSpec.describe Mihari::CLI do
     end
   end
 
+  describe "#dnpedia" do
+    before { allow(Mihari::Analyzers::DNPedia).to receive(:new).and_return(mock) }
+
+    it do
+      subject.start ["dnpedia", query]
+      expect(mock).to have_received(:run).once
+    end
+  end
+
   describe "#alerts" do
     let(:mock) { double("AlertViewer") }
     let(:alerts) {
