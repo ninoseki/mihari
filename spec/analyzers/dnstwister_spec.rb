0fcd4e7886fa2b3137be6d47118001eb869ccf9d
Manabu Niseki <manabu.niseki@gmail.com>
2019-12-10 19:06:34 +0900

    fix: fix an issue in dnstwister lookup
    Change to return domains which have A record

--- spec/analyzers/dnstwister_spec.rb
+++ spec/analyzers/dnstwister_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe Mihari::Analyzers::DNSTwister, :vcr do
   let(:query) { "example.com" }
 
   describe "#artifacts" do
+    before do
+      allow(Resolv).to receive(:getaddress).and_return("1.1.1.1")
+    end
+
     it do
       expect(subject.artifacts).to be_an(Array)
     end
