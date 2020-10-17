6957337c6f9bb64dbc574434501e9284a10263ad
Manabu Niseki <manabu.niseki@gmail.com>
2020-10-17 11:18:53 +0900

    feat: add urlscan Pro search

--- lib/mihari/analyzers/urlscan.rb
+++ lib/mihari/analyzers/urlscan.rb
@@ -9,18 +9,32 @@ module Mihari
       attr_reader :description
       attr_reader :query
       attr_reader :tags
+
+      attr_reader :filter
       attr_reader :target_type
+      attr_reader :use_pro
       attr_reader :use_similarity
 
-      def initialize(query, title: nil, description: nil, tags: [], target_type: "url", use_similarity: false)
+      def initialize(
+        query,
+        description: nil,
+        filter: nil,
+        tags: [],
+        target_type: "url",
+        title: nil,
+        use_pro: false,
+        use_similarity: false
+      )
         super()
 
         @query = query
         @title = title || "urlscan lookup"
         @description = description || "query = #{query}"
         @tags = tags
-        @target_type = target_type
 
+        @filter = filter
+        @target_type = target_type
+        @use_pro = use_pro
         @use_similarity = use_similarity
 
         raise InvalidInputError, "type should be url, domain or ip." unless valid_target_type?
@@ -48,6 +62,7 @@ module Mihari
 
       def search
         return api.pro.similar(query) if use_similarity
+        return api.pro.search(query: query, filter: filter, size: 10_000) if use_pro
 
         api.search(query, size: 10_000)
       end
