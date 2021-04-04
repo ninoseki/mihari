1690038df8894bb13bde7db8125fd88d77457899
Manabu Niseki <manabu.niseki@gmail.com>
2021-04-04 17:36:42 +0900

    test: add a basic test for the app

---
+++ spec/web/app_spec.rb
@@ -0,0 +1,19 @@
+ENV['APP_ENV'] = 'test'
+
+require 'rack/test'
+
+RSpec.describe Mihari::App do
+  include Rack::Test::Methods
+
+  def app
+    Mihari::App
+  end
+
+  it "returns 200" do
+    paths = %w(/ /api/alerts /api/tags /api/sources /api/config)
+    paths.each do |path|
+      get path
+      expect(last_response).to be_ok
+    end
+  end
+end
