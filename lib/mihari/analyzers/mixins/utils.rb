2e9af18a125f6839215d78713953ee0e43725d17
Manabu Niseki <manabu.niseki@gmail.com>
2021-06-10 19:01:09 +0900

    fix: dirty fix for Ruby 2.7

--- lib/mihari/analyzers/mixins/utils.rb
+++ lib/mihari/analyzers/mixins/utils.rb
@@ -3,6 +3,9 @@ module Mihari
     module Mixins
       module Utils
         def refang(indicator)
+          # for RSpec & Ruby 2.7
+          return indicator if indiactor.is_a?(Hash)
+
           indicator.gsub("[.]", ".").gsub("(.)", ".")
         end
       end
