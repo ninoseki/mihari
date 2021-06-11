605dc7fd1332172956be1169bec669001c289383
Manabu Niseki <manabu.niseki@gmail.com>
2021-06-11 17:53:04 +0900

    feat: add schema for rule

---
+++ lib/mihari/schemas/rule.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+require "dry/schema"
+require "dry/validation"
+require "dry/types"
+
+require "mihari/schemas/macros"
+
+module Mihari
+  module Types
+    include Dry.Types()
+  end
+
+  DataTypes = Types::String.enum(*ALLOWED_DATA_TYPES)
+
+  AnalyzerTypes = Types::String.enum(
+    "binaryedge", "censys", "circl", "dnpedia", "dnstwister",
+    "onyphe", "otx", "passivetotal", "pulsedive", "securitytrails",
+    "shodan", "virustotal"
+  )
+
+  module Schemas
+    Analyzer = Dry::Schema.Params do
+      required(:service).value(AnalyzerTypes)
+      required(:query).value(:string)
+    end
+
+    Spyse = Dry::Schema.Params do
+      required(:service).value(Types::String.enum("spyse"))
+      required(:query).value(:string)
+      required(:type).value(Types::String.enum("ip", "domain"))
+    end
+
+    ZoomEye = Dry::Schema.Params do
+      required(:service).value(Types::String.enum("zoomeye"))
+      required(:query).value(:string)
+      required(:type).value(Types::String.enum("host", "web"))
+    end
+
+    Crtsh = Dry::Schema.Params do
+      required(:service).value(Types::String.enum("crtsh"))
+      required(:query).value(:string)
+      optional(:exclude_expired).value(:bool).default(true)
+    end
+
+    Urlscan = Dry::Schema.Params do
+      required(:service).value(Types::String.enum("urlscan"))
+      required(:query).value(:string)
+      optional(:use_similarity).value(:bool).default(true)
+    end
+
+    Rule = Dry::Schema.Params do
+      required(:title).value(:string)
+      required(:description).value(:string)
+
+      optional(:tags).value(array[:string]).default([])
+      optional(:id).value(:string)
+
+      optional(:author).value(:string)
+      optional(:created_at).value(:date)
+      optional(:updated_at).value(:date)
+
+      required(:queries).value(:array).each { Analyzer | Spyse | ZoomEye | Urlscan | Crtsh }
+
+      optional(:allowed_data_types).value(array[DataTypes]).default(ALLOWED_DATA_TYPES)
+    end
+
+    class RuleContract < Dry::Validation::Contract
+      params(Rule)
+    end
+  end
+end
---
+++ spec/schemas/rule_spec.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require "yaml"
+require "active_support/core_ext/hash"
+
+def symbolize_data(data)
+  data.symbolize_keys!
+
+  queries = data[:queries]
+  queries.each(&:symbolize_keys!)
+
+  data
+end
+
+RSpec.describe Mihari::Schemas::Rule do
+  let(:contract) { Mihari::Schemas::RuleContract.new }
+
+  context "with valid rule" do
+    it do
+      result = contract.call(description: "foo", title: "foo", queries: [{ service: "shodan", query: "foo" }])
+      expect(result.errors.empty?).to eq(true)
+    end
+
+    it do
+      result = contract.call(
+        description: "foo",
+        title: "foo",
+        queries: [
+          { service: "shodan", query: "foo" },
+          { service: "crtsh", query: "foo", exclude_expired: true },
+          { service: "zoomeye", query: "foo", type: "host" },
+        ]
+      )
+      expect(result.errors.empty?).to eq(true)
+    end
+
+    it do
+      result = contract.call(
+        description: "foo",
+        title: "foo",
+        queries: [
+          { service: "shodan", query: "foo" },
+        ],
+        allowed_data_types: ["ip"]
+      )
+      expect(result.errors.empty?).to eq(true)
+    end
+  end
+
+  context "with invalid service name" do
+    it do
+      expect { contract.call(description: "foo", title: "foo", queries: [{ service: "foo", query: "foo" }]) }.to raise_error(NoMethodError)
+    end
+
+    it do
+      expect do
+        contract.call(
+          description: "foo",
+          title: "foo",
+          queries: [
+            { service: "shodan", query: "foo" },
+            { service: "crtsh", query: "foo", exclude_expired: 1 }, # should be bool
+            { service: "zoomeye", query: "foo", type: "bar" }, # should be any of host or web
+          ]
+        )
+      end.to raise_error(NoMethodError)
+    end
+
+    it do
+      result = contract.call(
+        description: "foo",
+        title: "foo",
+        queries: [
+          { service: "shodan", query: "foo" },
+        ],
+        allowed_data_types: ["foo"] # should be any of ip, domain, mail, url or hash
+      )
+      expect(result.errors.empty?).to eq(false)
+    end
+  end
+end
