e98a6f539a654343036643fd1b77026649d3de8b
Manabu Niseki <manabu.niseki@gmail.com>
2021-09-03 07:33:59 +0900

    feat: add IPInfo based enricher

---
+++ spec/enrichers/ipinfo_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+RSpec.describe Mihari::Enrichers::IPInfo, vcr: "Mihari_Enrichers_IPInfo/ip:1.1.1.1" do
+  subject { described_class }
+
+  describe ".query" do
+    let(:ip) { "1.1.1.1" }
+
+    it do
+      res = subject.query(ip)
+      expect(res.asn).to eq(13_335)
+      expect(res.country_code).to eq("US")
+      expect(res.hostname).to eq("one.one.one.one")
+    end
+  end
+end
