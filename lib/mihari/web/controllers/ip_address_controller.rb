765e734e7c1b8c2433c74e6b86cc489ec78228d3
Manabu Niseki <manabu.niseki@gmail.com>
2021-09-03 07:34:48 +0900

    refactor: use IPInfo enricher

--- lib/mihari/web/controllers/ip_address_controller.rb
+++ lib/mihari/web/controllers/ip_address_controller.rb
@@ -1,34 +1,19 @@
 # frozen_string_literal: true
 
-require "http"
-require "json"
-
 module Mihari
   module Controllers
     class IPAddressController < BaseController
-      def query(ip)
-        headers = {}
-        token = Mihari.config.ipinfo_api_key
-        unless token.nil?
-          headers[:authorization] = "Bearer #{token}"
-        end
-
-        res = HTTP.headers(headers).get("https://ipinfo.io/#{ip}/json")
-        JSON.parse res.to_s
-      end
-
       get "/api/ip_addresses/:ip" do
         param :ip, String, required: true
 
         ip = params["ip"].to_s
 
-        begin
-          data = query(ip)
-          json data
-        rescue HTTP::Error
+        data = Enrichers::IPInfo.query(ip)
+        if data.nil?
           status 404
-
           json({ message: "IP:#{ip} is not found" })
+        else
+          json data.to_hash
         end
       end
     end
