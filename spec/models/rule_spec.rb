b1d332414ede508958973cd7b32fbbf39531fa1d
Manabu Niseki <manabu.niseki@gmail.com>
2022-01-16 18:33:13 +0900

    test: add spec for Mihari::Rule

---
+++ spec/models/rule_spec.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+RSpec.describe Mihari::Rule do
+  include_context "with database fixtures"
+
+  describe ".search" do
+    it do
+      alerts = described_class.search(Mihari::Structs::Rule::SearchFilterWithPagination.new)
+      expect(alerts.length).to eq(2)
+    end
+
+    it do
+      alerts = described_class.search(Mihari::Structs::Rule::SearchFilterWithPagination.new(tag_name: "tag1"))
+      expect(alerts.length).to eq(1)
+    end
+
+    it do
+      alerts = described_class.search(Mihari::Structs::Rule::SearchFilterWithPagination.new(tag_name: "404_not_found"))
+      expect(alerts.length).to eq(0)
+    end
+
+    it do
+      alerts = described_class.search(Mihari::Structs::Rule::SearchFilterWithPagination.new(title: "404_not_found"))
+      expect(alerts.length).to eq(0)
+    end
+
+    it do
+      alerts = described_class.search(Mihari::Structs::Rule::SearchFilterWithPagination.new(title: "test"))
+      expect(alerts.length).to eq(1)
+    end
+
+    it do
+      alerts = described_class.search(Mihari::Structs::Rule::SearchFilterWithPagination.new(description: "404_not_found"))
+      expect(alerts.length).to eq(0)
+    end
+
+    it do
+      alerts = described_class.search(Mihari::Structs::Rule::SearchFilterWithPagination.new(description: "test"))
+      expect(alerts.length).to eq(2)
+    end
+  end
+
+  describe ".count" do
+    it do
+      count = described_class.count(Mihari::Structs::Rule::SearchFilter.new)
+      expect(count).to eq(2)
+    end
+
+    it do
+      count = described_class.count(Mihari::Structs::Rule::SearchFilter.new(tag_name: "tag1"))
+      expect(count).to eq(1)
+    end
+
+    it do
+      count = described_class.count(Mihari::Structs::Rule::SearchFilter.new(tag_name: "404_not_found"))
+      expect(count).to eq(0)
+    end
+
+    it do
+      count = described_class.count(Mihari::Structs::Rule::SearchFilter.new(title: "404_not_found"))
+      expect(count).to eq(0)
+    end
+
+    it do
+      count = described_class.count(Mihari::Structs::Rule::SearchFilter.new(title: "test"))
+      expect(count).to eq(1)
+    end
+
+    it do
+      count = described_class.count(Mihari::Structs::Rule::SearchFilter.new(description: "404_not_found"))
+      expect(count).to eq(0)
+    end
+
+    it do
+      count = described_class.count(Mihari::Structs::Rule::SearchFilter.new(description: "test"))
+      expect(count).to eq(2)
+    end
+  end
+end
