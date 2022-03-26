1b89b3509c87d8cac4f5b322f2c3a2c2f9931820
Manabu Niseki <manabu.niseki@gmail.com>
2022-03-26 11:08:59 +0900

    feat: add "yaml" column to rules table
    Add "yaml" column to rules table to make possible to use ENV vars in a rule

--- lib/mihari/database.rb
+++ lib/mihari/database.rb
@@ -112,6 +112,12 @@ class AddeMetadataToArtifactSchema < ActiveRecord::Migration[7.0]
   end
 end
 
+class AddYAMLToRulesSchema < ActiveRecord::Migration[7.0]
+  def change
+    add_column :rules, :yaml, :text, if_not_exists: true
+  end
+end
+
 def adapter
   return "postgresql" if Mihari.config.database.start_with?("postgresql://", "postgres://")
   return "mysql2" if Mihari.config.database.start_with?("mysql2://")
@@ -137,9 +143,11 @@ module Mihari
           AddeSourceToArtifactSchema,
           EnrichmentsSchema,
           EnrichmentCreatedAtSchema,
-          # v4
+          # v4.0
           RuleSchema,
-          AddeMetadataToArtifactSchema
+          AddeMetadataToArtifactSchema,
+          # v4.4
+          AddYAMLToRulesSchema
         ].each { |schema| schema.migrate direction }
       end
       memoize :migrate unless test_env?
--- spec/mixins/rule_spec.rb
+++ spec/mixins/rule_spec.rb
@@ -15,12 +15,25 @@ RSpec.describe Test do
       filename = "/tmp/foo.yml"
       subject.initialize_rule_yaml(filename, files)
 
-      data = YAML.safe_load(files.read(filename), permitted_classes: [Date], symbolize_names: true)
-
-      rule = Mihari::Structs::Rule::Rule.new(data)
+      yaml = files.read(filename)
+      rule = Mihari::Structs::Rule::Rule.from_yaml(yaml)
       expect(rule.errors?).to eq(false)
 
-      expect(data).to be_a(Hash)
+      expect(rule.data).to be_a(Hash)
+    end
+  end
+
+  describe "#load_yaml_from_db" do
+    include_context "with database fixtures"
+
+    it do
+      rule = Mihari::Rule.first
+      yaml = subject.load_yaml_from_db rule.id
+
+      # Rule fixture does not have yaml data
+      expect(rule.yaml).to eq(nil)
+      # Then the function should return YAML based on "data"
+      expect(yaml).is_a?(String)
     end
   end
 end
