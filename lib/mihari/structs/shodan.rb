9aab21f0c4fcf03e62ab3aac13caea8e3597929e
Manabu Niseki <manabu.niseki@gmail.com>
2022-04-21 07:17:36 +0900

    refactor: collect ports and hostnames from matches

--- lib/mihari/structs/shodan.rb
+++ lib/mihari/structs/shodan.rb
@@ -22,6 +22,7 @@ module Mihari
         attribute :location, Location
         attribute :domains, Types.Array(Types::String)
         attribute :ip_str, Types::String
+        attribute :port, Types::Integer
         attribute :metadata, Types::Hash
 
         def self.from_dynamic!(d)
@@ -40,6 +41,7 @@ module Mihari
             location: Location.from_dynamic!(d.fetch("location")),
             domains: d.fetch("domains"),
             ip_str: d.fetch("ip_str"),
+            port: d.fetch("port"),
             metadata: d
           )
         end
