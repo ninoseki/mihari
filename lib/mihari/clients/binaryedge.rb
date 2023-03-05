891c7d1b3c9f8ed512590779a5ca953860ad1b5c
Manabu Niseki <manabu.niseki@gmail.com>
2023-03-05 17:57:30 +0900

    refactor: remove "binaryedge" gem

---
+++ lib/mihari/clients/binaryedge.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Mihari
+  module Clients
+    class BinaryEdge < Base
+      def initialize(base_url = "https://api.binaryedge.io/v2", api_key:, headers: {})
+        raise(ArgumentError, "'api_key' argument is required") unless api_key
+
+        headers["x-key"] = api_key
+
+        super(base_url, headers: headers)
+      end
+
+      #
+      # @param [String] query String used to query our data
+      # @param [Integer] page Default 1, Maximum: 500
+      # @param [Integer, nil] only_ips If selected, only output IP addresses, ports and protocols.
+      #
+      # @return [Hash]
+      #
+      def search(query, page: 1, only_ips: nil)
+        params = {
+          query: query,
+          page: page,
+          only_ips: only_ips
+        }.compact
+
+        res = get("/query/search", params: params)
+        JSON.parse(res.body.to_s)
+      end
+    end
+  end
+end
