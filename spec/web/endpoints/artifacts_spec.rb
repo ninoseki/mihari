9dc3f0652f469c4928c86f0712cd31fddb8f79c8
Manabu Niseki <manabu.niseki@gmail.com>
2023-10-08 15:02:02 +0900

    fix: mock enrich method

--- spec/web/endpoints/artifacts_spec.rb
+++ spec/web/endpoints/artifacts_spec.rb
@@ -33,11 +33,18 @@ RSpec.describe Mihari::Endpoints::Artifacts, :vcr do
 
     context "with enriched artifact" do
       before do
-        @enriched_domain_artifact = Mihari::Artifact.new(data: "example.com", alert_id: alert.id)
+        allow(Mihari::DnsRecord).to receive(:build_by_domain).and_return(
+          [Mihari::DnsRecord.new(resource: "A", value: Faker::Internet.unique.ip_v4_address)]
+        )
+        allow(Mihari::ReverseDnsName).to receive(:build_by_ip).and_return(
+          [Mihari::ReverseDnsName.new(name: Faker::Internet.unique.domain_name)]
+        )
+
+        @enriched_domain_artifact = Mihari::Artifact.new(data: Faker::Internet.unique.domain_name, alert_id: alert.id)
         @enriched_domain_artifact.enrich_dns
         @enriched_domain_artifact.save
 
-        @enriched_ip_artifact = Mihari::Artifact.new(data: "1.1.1.1", alert_id: alert.id)
+        @enriched_ip_artifact = Mihari::Artifact.new(data: Faker::Internet.unique.ip_v4_address, alert_id: alert.id)
         @enriched_ip_artifact.enrich_reverse_dns
         @enriched_ip_artifact.save
       end
