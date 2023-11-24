ed451d81b51a022741ae12a4a3a62bde2c3f8cb8
Manabu Niseki <manabu.niseki@gmail.com>
2023-11-24 17:17:54 +0900

    test: renew tests

--- spec/models/geolocation_spec.rb
+++ spec/models/geolocation_spec.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.describe Mihari::Models::Geolocation, vcr: "Mihari_Enrichers_IPInfo/ip:1.1.1.1" do
-  subject(:klass) { described_class }
-
   describe ".build_by_ip" do
     let!(:ip) { "1.1.1.1" }
 
     it do
-      geolocation = klass.build_by_ip(ip)
+      geolocation = described_class.build_by_ip(ip)
       expect(geolocation.country_code).to eq("US")
       expect(geolocation.country).to eq("United States")
     end
