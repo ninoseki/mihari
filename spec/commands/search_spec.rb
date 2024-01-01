d959763add5990fee96363dde2c4f267af471383
Manabu Niseki <manabu.niseki@gmail.com>
2024-01-01 10:34:35 +0900

    fix: use command spec as an integration spec

--- spec/commands/search_spec.rb
+++ spec/commands/search_spec.rb
@@ -4,20 +4,21 @@ class SearchCLI < Mihari::CLI::Base
   include Mihari::Commands::Search
 end
 
-RSpec.describe Mihari::Commands::Search do
+RSpec.describe Mihari::Commands::Search, :vcr do
   include_context "with mocked logger"
 
   let!(:path) { File.expand_path("../fixtures/rules/valid_rule.yml", __dir__) }
   let!(:yaml) { File.read path }
   let!(:rule) { Mihari::Rule.from_yaml yaml }
 
-  describe "#search" do
-    before do
-      spy = spy(Mihari::Rule)
-      allow(spy).to receive(:call).and_return FactoryBot.build(:alert, rule: rule.model)
-      allow(Mihari::Rule).to receive(:new).and_return(spy)
-    end
+  before do
+    double = class_double(Mihari::Models::WhoisRecord)
+    allow(double).to receive(:build_by_domain).and_return(nil)
 
+    allow(Parallel).to receive(:processor_count).and_return(0)
+  end
+
+  describe "#search" do
     it do
       expect { SearchCLI.start ["search", "-f", path] }.to output(include(rule.id)).to_stdout
     end
