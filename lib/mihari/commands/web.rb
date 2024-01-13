1966796b887ad89c3bdb6289504a9be45a93dc39
Manabu Niseki <manabu.niseki@gmail.com>
2024-01-13 09:58:14 +0900

    refactor: renew Puma desc & default

--- lib/mihari/commands/web.rb
+++ lib/mihari/commands/web.rb
@@ -10,10 +10,10 @@ module Mihari
         def included(thor)
           thor.class_eval do
             desc "web", "Start the web app"
-            method_option :port, type: :numeric, default: 9292, desc: "Hostname to listen on"
-            method_option :host, type: :string, default: "localhost", desc: "Port to listen on"
+            method_option :port, type: :numeric, default: 9292, desc: "Port to listen on"
+            method_option :host, type: :string, default: "localhost", desc: "Hostname to listen on"
             method_option :threads, type: :string, default: "0:5", desc: "min:max threads to use"
-            method_option :verbose, type: :boolean, default: true, desc: "Report each request"
+            method_option :verbose, type: :boolean, default: false, desc: "Don't report each request"
             method_option :worker_timeout, type: :numeric, default: 60, desc: "Worker timeout value (in seconds)"
             method_option :open, type: :boolean, default: true, desc: "Whether to open the app in browser or not"
             method_option :env, type: :string, default: "production", desc: "Environment"
