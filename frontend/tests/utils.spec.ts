a82cb5ceb39c4a3e127d97036e3ba9c53d4d9391
Manabu Niseki <manabu.niseki@gmail.com>
2024-01-14 10:18:13 +0900

    chore: apply ESLint & Prettier

--- frontend/tests/utils.spec.ts
+++ frontend/tests/utils.spec.ts
@@ -1,9 +1,9 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it } from "vitest"
 
-import { getHumanizedRelativeTime } from '@/utils'
+import { getHumanizedRelativeTime } from "@/utils"
 
-describe('getHumanizedRelativeTime', () => {
-  it('returns a relative time in humanized format', () => {
-    expect(getHumanizedRelativeTime('1970-01-01 00:00:00')).toContain('years')
+describe("getHumanizedRelativeTime", () => {
+  it("returns a relative time in humanized format", () => {
+    expect(getHumanizedRelativeTime("1970-01-01 00:00:00")).toContain("years")
   })
 })
